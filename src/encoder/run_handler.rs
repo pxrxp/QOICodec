5ee8ca1e9b59e2da4672ac92f5c7192c8fe76ff5
pxrxp <187556078+pxrxp@users.noreply.github.com>
2025-11-25 17:06:32 +0545

    Fix run_handler bug which was ignoring 63rd run pixel

--- src/encoder/run_handler.rs
+++ src/encoder/run_handler.rs
@@ -2,17 +2,17 @@ use crate::{QOIState, encoder::ImageBuffer};
 use image::Rgba;
 
 pub fn handle(qoi_buffer: &mut ImageBuffer, state: &mut QOIState, pixel: &Rgba<u8>) -> bool {
-    let run_increment = if state.run_length + 1 > 62 { 0 } else { 1 };
-
     if *pixel == state.prev_pixel {
-        state.run_length += run_increment;
+        state.run_length += 1;
+
+        if state.run_length == 62 {
+            qoi_buffer.add_run_pixels(state.run_length);
+            state.run_length = 0;
+        }
         return true;
     }
 
-    let run_length_limit_exceeded = *pixel == state.prev_pixel && run_increment == 0;
-    let pixel_changed = *pixel != state.prev_pixel && state.run_length != 0;
-
-    if run_length_limit_exceeded || pixel_changed {
+    if state.run_length > 0 {
         qoi_buffer.add_run_pixels(state.run_length);
         state.run_length = 0;
     }
